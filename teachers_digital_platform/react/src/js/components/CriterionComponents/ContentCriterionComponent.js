9b2eba8e62d8cabfa307f9442a3b0ed89b90dbcf
Allen Tuggle <allen.tuggle@excella.com>
2018-03-23 03:18:31 +0000

    REACT: WIP: Not working, built and added to crt-base.html

--- teachers_digital_platform/react/src/js/components/CriterionComponents/ContentCriterionComponent.js
+++ teachers_digital_platform/react/src/js/components/CriterionComponents/ContentCriterionComponent.js
@@ -3,18 +3,44 @@ import React from "react";
 import CriterionRowComponent from "./CriterionRowComponent";
 
 export default class ContentCriterionComponent extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-
-        }
+    changeContentText(e) {
+        const contentText = e.target.value;
+        this.props.changeContentText(contentText);
     }
+
+    changeContentRadio(e) {
+        this.props.changeContentRadio("In Progress...");
+    }   
 
     render () {
         return (
-            <div>
-                <p>
-                    This is a test Comment to see if this works
+            <div className="CriterionPageTextFormat">
+                <ol>
+                    <li>
+                        <h4>Indicator</h4>
+                        <p>People save for the future, and might have different goals for saving and make different choices about how to save.</p>
+                        <h4>Component</h4>
+                        <p>One can save money in various ways, including at home in a piggy bank or at a commercial bank, credit union, or savings and loan institution.</p>
+                        <p>Saving helps individuals act on future opportunities, meet short-term and long-term goals, and address financial emergencies.</p>
+                        <div className="m-form-field m-form-field__radio">
+                            <input className="a-radio" type="radio" name="radio_1" id="test_1" onChange={this.changeContentRadio.bind(this)} />
+                            <label className="a-label" >Yes</label>
+                        </div>
+                        <div className="m-form-field m-form-field__radio">
+                            <input className="a-radio" type="radio" name="radio_1" id="test_2" onChange={this.changeContentRadio.bind(this)} />
+                            <label className="a-label" >No</label>
+                        </div>
+                    </li>
+                </ol>
+
+
+                <h3>My notes</h3>
+                <p className="m-form-field m-form-field__textarea">
+                    <label className="a-label a-label__heading">
+                        Write your notes for this criterion <em>(Optional)</em>
+                    </label>
+                    <br />
+                    <textarea className="a-text-input" id="test_7" value={this.props.contentText} onChange={this.changeContentText.bind(this)} ></textarea>
                 </p>
             </div>
         )
--- teachers_digital_platform/react/src/js/components/pages/ContentCriterionPage.js
+++ teachers_digital_platform/react/src/js/components/pages/ContentCriterionPage.js
@@ -1,47 +1,30 @@
 import React from "react";
 
-export default class ContentCriterionPage extends React.Component {
-    changeContentText(e) {
-        const contentText = e.target.value;
-        this.props.changeContentText(contentText);
-    }
+import ContentCriterion from "../CriterionComponents/ContentCriterionComponent";
 
-    changeContentRadio(e) {
-        this.props.changeContentRadio("In Progress...");
-    }   
-    
+export default class ContentCriterionPage extends React.Component {
+
     render() {
         return (
-            <div className="CriterionPageTextFormat">
-              <div className="block">
-                  <h1>Criterion 1: {this.props.currentPage}</h1>
-                  <p>Does the curriculum address grade-level appropriate topics for saving and investing? Select yes if the following components are addressed. Select no if the following components are not addressed.</p>
-                  <ol>
-                      <li>
-                          <h4>Indicator</h4>
-                          <p>People save for the future, and might have different goals for saving and make different choices about how to save.</p>
-                          <h4>Component</h4>
-                          <p>One can save money in various ways, including at home in a piggy bank or at a commercial bank, credit union, or savings and loan institution.</p>
-                          <p>Saving helps individuals act on future opportunities, meet short-term and long-term goals, and address financial emergencies.</p>
-                          <div className="m-form-field m-form-field__radio">
-                              <input className="a-radio" type="radio" name="radio_1" id="test_1" onChange={this.changeContentRadio.bind(this)} />
-                              <label className="a-label" >Yes</label>
-                          </div>
-                          <div className="m-form-field m-form-field__radio">
-                              <input className="a-radio" type="radio" name="radio_1" id="test_2" onChange={this.changeContentRadio.bind(this)} />
-                              <label className="a-label" >No</label>
-                          </div>
-                      </li>
-                  </ol>
-                  <h3>My notes</h3>
-                  <p className="m-form-field m-form-field__textarea">
-                      <label className="a-label a-label__heading">
-                          Write your notes for this criterion <em>(Optional)</em>
-                      </label>
-                      <br />
-                      <textarea className="a-text-input" id="test_7" value={this.props.contentText} onChange={this.changeContentText.bind(this)} ></textarea>
-                  </p>
-              </div>
+            <div>
+                <h1 className="block">{this.props.currentPage}</h1>
+                <div className="CriterionPageTextFormat">
+                    <h1>Criterion 1</h1>
+                    <p>Does the curriculum address grade-level appropriate topics for saving and investing? Select yes if the following components are addressed. Select no if the following components are not addressed.</p>
+
+                    <ContentCriterion currentPage={this.props.currentPage}
+                            contentText={this.props.contentText}
+                            changeContentText={this.props.changeContentText}
+                            changeContentRadio={this.props.changeContentRadio}/>
+
+                    <h1>Criterion 2: {this.props.currentPage}</h1>
+                    <p>Does the curriculum address grade-level appropriate topics for saving and investing? Select yes if the following components are addressed. Select no if the following components are not addressed.</p>
+
+                    <ContentCriterion currentPage={this.props.currentPage}
+                            contentText={this.props.contentText}
+                            changeContentText={this.props.changeContentText}
+                            changeContentRadio={this.props.changeContentRadio}/>
+                </div>
             </div>
         );
     }
