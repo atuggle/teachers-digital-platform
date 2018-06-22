b946d7538cacb5d6f6c9cb753ba219c9638c0682
Will Krause <wkrause1@radford.edu>
2018-06-22 20:07:21 +0000

    Added test tdp page to models

--- teachers_digital_platform/models.py
+++ teachers_digital_platform/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, TabbedInterface, ObjectList
+from v1.models import CFGOVPage, CFGOVPageManager
+
+
+class TDPPage(CFGOVPage):
+    body = RichTextField(blank=True)
+    intro = models.CharField(max_length=250)
+
+    edit_handler = TabbedInterface([
+        ObjectList(Page.content_panels + [ 
+            FieldPanel('body', classname="full"),
+            FieldPanel('intro', classname="full")],
+            heading='General Content'),
+        ObjectList(CFGOVPage.sidefoot_panels, heading='Sidebar/Footer'),
+        ObjectList(CFGOVPage.settings_panels, heading='Configuration'),
+    ])
+
+    objects = CFGOVPageManager()
